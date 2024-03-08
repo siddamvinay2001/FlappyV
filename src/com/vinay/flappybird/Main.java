fa0cdf52ca4fa7db20b350cbaf3ec55cde1d0032
siddamvinay2001 <siddamvinay1368@gmail.com>
2024-03-08 15:08:56 +0530

    feat: Create Window

--- src/com/vinay/flappybird/Main.java
+++ src/com/vinay/flappybird/Main.java
@@ -15,24 +15,31 @@ public class Main implements Runnable {
     private long window;
     
     public void start() {
-        running = true; // Corrected to set running to true
+        running = true; 
         thread = new Thread(this,"Game");
         thread.start();
     }
     
     private void init() {
         
-        if(!glfwInit()) { // Corrected to compare with GLFW_TRUE
+        if(!glfwInit()) { 
             // Error handling
             throw new IllegalStateException("Unable to initialize GLFW");
         }
         glfwWindowHint(GLFW_RESIZABLE,GL_TRUE);
         window = glfwCreateWindow(width, height ,"Flappy", NULL, NULL);
         
+        if(window == NULL) {
+        	//ERROR HANDLE
+        	return;
+        }
+        
         // Getting primary monitor video mode for handling windows resolution
         long primaryMonitor = glfwGetPrimaryMonitor();
         GLFWVidMode vidMode = glfwGetVideoMode(primaryMonitor);
-        
+        glfwSetWindowPos(window, (vidMode.width() - width) / 2, (vidMode.height() - height) / 2);
+        glfwMakeContextCurrent(window);
+        glfwShowWindow(window);
     }
     
     public void run() {
@@ -40,15 +47,19 @@ public class Main implements Runnable {
         while(running) {
             update();
             render();
+            
+            if(glfwWindowShouldClose(window)) {
+            	running = false;
+            }
         }
     }
     
     private void update() {
-        
+        glfwPollEvents();
     }
     
     private void render() {
-        
+        glfwSwapBuffers(window);
     }
 
     public static void main(String[] args) {
